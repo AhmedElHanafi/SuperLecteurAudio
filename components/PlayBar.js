c0ed2eef483864740e90cc536cf94d2b0c766fe0
AhmedElHanafi <71706191+AhmedElHanafi@users.noreply.github.com>
2023-11-27 15:06:52 +0100

    ajout de l'Equilizer

--- components/PlayBar.js
+++ components/PlayBar.js
@@ -1,3 +1,5 @@
+import "../libs/webaudiocontrols.js";
+
 export class PlayBar extends HTMLElement {  
     constructor() {
       super();
@@ -24,13 +26,54 @@ export class PlayBar extends HTMLElement {
         height: 90%;
         padding: 0 5px;
     }    
+    
+    .slider-container {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+     }
+     
+     .slider {
+      -webkit-appearance: slider-vertical;
+      width: 25px;
+      height: 100px;
+      background: #d3d3d3;
+      outline: none;
+     }
+     input{
+      padding:15px;
+     }
+     p{
+      padding-left : 5px;
+     }
+     
+     .slider:hover {
+      background: #999;
+     }
+     .knob{
+      background-color = "white";
+     }
       </style>
-      <div class="player">
-      <h1> Equilizer</h1>
-
-        <input type="range" min="1" max="100" value="50" class="slider" id="myRange1" orient="vertical">
-        <input type="range" min="1" max="100" value="50" class="slider" id="myRange2" orient="vertical">
-        <input type="range" min="1" max="100" value="50" class="slider" id="myRange" orient="vertical">
+      <div class="player" style="display: flex; flex-direction: column;">
+        <div class="slider-container">
+          <input type="range" min="1" max="100" value="50" class="slider" id="slider1">
+          <input type="range" min="1" max="100" value="50" class="slider" id="slider2">
+          <input type="range" min="1" max="100" value="50" class="slider" id="slider3">
+        </div>
+        <br>
+        <div class="slider-labels" style="display: flex; flex-direction: row;">
+          <p id="label1">Volume</p>
+          <p id="label2">Balance</p>
+          <p id="label3">Frequency</p>
+        </div>
+        <div class="knob">
+        <webaudio-knob id="knob"  
+        src="../images/LittlePatty.png" 
+        value="50" step="1" 
+        diameter="64" 
+        tooltip="Knob1 tooltip %d">
+        </webaudio-knob>
+        </div>
       </div>
       `;
     }
@@ -39,6 +82,7 @@ export class PlayBar extends HTMLElement {
     }
   
     defineListeners() {
+      //je veux recuperer les valeur des slider a fin de les utiliser
     }
   }
   
